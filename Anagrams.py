428584a8cbfee65f9b34a05dc05e999efa1d659d
Krish Ganotra <krishganotra@gmail.com>
2021-12-31 01:53:54 -0500

    Anagrams (GamePigeon) solver

---
+++ Anagrams.py
@@ -0,0 +1,20 @@
+from itertools import permutations
+
+filename = "scrabble.txt"
+dict = {line.lower().strip() for line in open(filename)} # can use set where key = first letter of word
+dict = {x for x in dict if len(x) > 2}
+
+letters = input("What are your letters: ").lower().strip()
+assert len(letters) == 6
+
+openset = [("", letters)]
+while(openset):
+    curr,left = openset.pop(0)
+    if (curr in dict):
+        print(curr)
+    for i,c in enumerate(left):
+        new_word = curr + c
+        new_left = left[:i] + left[i+1:]
+        openset.append((new_word, new_left))
+        
+
