ad22f02bf43227cdfb076eca8a3279141fe42f9a
Krish Ganotra <krishganotra@gmail.com>
2022-02-11 16:16:17 -0500

    Updated Anagrams to remove duplicates and print in length order

--- Anagrams/Anagrams.py
+++ Anagrams/Anagrams.py
@@ -1,6 +1,3 @@
-#%% Import Libraries
-from itertools import permutations
-
 #%% Read in dictionary
 filename = "../scrabble.txt"
 dict = {line.lower().strip() for line in open(filename)} # can use set where key = first letter of word
@@ -10,15 +7,21 @@ dict = {x for x in dict if len(x) > 2}
 letters = input("What are your letters: ").lower().strip()
 assert len(letters) == 6
 
-#%% BFS + print as you go
+all_words = set()
+#%% BFS
 openset = [("", letters)]
 while(openset):
     curr,left = openset.pop(0)
     if (curr in dict):
-        print(curr)
+        all_words.add(curr)
     for i,c in enumerate(left):
         new_word = curr + c
         new_left = left[:i] + left[i+1:]
         openset.append((new_word, new_left))
+
+#%% Output
+output = list(all_words)
+output.sort(key=lambda x: len(x))
+print("\n".join(output))
         
 
