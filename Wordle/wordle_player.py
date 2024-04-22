d494d4e159f0e839e9396df8a06a852bdab49933
Krish Ganotra <krishganotra@gmail.com>
2024-04-22 13:40:52 -0400

    Better formatted UI for wordle player

--- Wordle/wordle_player.py
+++ Wordle/wordle_player.py
@@ -8,5 +8,19 @@ while True:
     result = input("What was your result (= for green, + for yellow, - for grey): ")
     answers = filter_answers(answers, ans_by_letter, *read_result(result, guess))
     print(f"There are {len(answers)} possible answers left")
-    print("Your best guesses are: ", play_turn(answers, valid_guesses, ans_by_letter)[:5])
+    
+    print()
+    if(len(answers) == 1):
+        print(f"The wordle word of the day is: {answers}")
+    else:
+        print("The best guesses are:")
+        top_guess =  play_turn(answers, valid_guesses, ans_by_letter)[:5]
+        print("Word\t\t Avg # of guesses left")
+        print("-"*40)
+        for avg, word in top_guess:
+            print(f"{word}\t\t|\t {avg:.3f}")
+            
+    print("\n"*2)
+    # print("Your best guesses are: ",)
+    # print()
     
